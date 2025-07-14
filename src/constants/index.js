de04607dacd2dc568608e29d398818af77546445
Manisha Magar <magarmanisha248@gmail.com>
2025-07-15 04:49:47 +0545

    feat:feat adding nav bar

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/_manimom_/" },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/magar-manisha/" },
+  { name: "GitHub", href: "https://github.com/magar-manisha" },
+];

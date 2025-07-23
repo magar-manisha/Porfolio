3387223a660ec51ee52d92242ecd5ba53365a8b0
Manisha Magar <magarmanisha248@gmail.com>
2025-07-23 06:22:57 +0545

    feat: service section

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Work = () => {
   return (
-    <section id='work'>Work</section>
-  )
-}
+    <section id="work" className="min-h-screen bg-black">
+      <div className="px-20 pt-20">
+        <h1 className="text-7xl text-white">SELECTED WORKS ...</h1>
 
-export default Work
+      </div>
+      
+    </section>
+  );
+};
+
+export default Work;

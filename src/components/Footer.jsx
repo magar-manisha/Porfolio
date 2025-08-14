790260bc483c5514743b2f5e7da69a22b2b3ebcb
Manisha Magar <magarmanisha248@gmail.com>
2025-08-15 05:01:23 +0545

    chrore:making footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import {LocalTime} from "../utils/helperfunction";
 
 const Footer = () => {
   return (
-    <div className="px-10 pb-7 pt-10 flex items-center justify-between">
-        <div className="flex gap-10">
+    <div className="px-5 md:px-10 pb-7 md:pt-10 pt-5 flex flex-col md:flex-row md:items-center md:justify-between gap-y-5 md:gap-y-0">
+        <div className="flex justify-between md:gap-10">
          <div>
         <p className="text-sm text-text-100 pb-2">VESRSION</p>
         <p className="text-sm">2025 Edition</p>

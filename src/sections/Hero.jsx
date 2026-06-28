4b461e348e526b4b0c8120c7f327a00f3336d602
Manisha Magar <magarmanisha248@gmail.com>
2026-06-28 21:45:29 +0545

    feat: enhance the ui

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
 
       {/* body */}
-      <div className="mt-10 md:mt-20 2xl:mt-[20%] flex flex-col justify-between flex-1">
+      <div className="flex flex-col justify-end flex-1">
         {/* Mobile: 2 lines */}
         <div className="md:hidden flex flex-col w-full px-7 transform scale-y-120 origin-bottom">
           <p className="text-black font-medium leading-none tracking-tighter text-[clamp(2.5rem,20vw,6rem)]">
@@ -62,38 +62,35 @@ const Hero = () => {
             </span>
           ))}
         </div>
+      </div>
 
-        <div className="flex flex-col-reverse gap-y-5 mt-5 md:flex-row md:items-end  w-full px-7 md:px-0 h-full flex-1">
-          <p className="w-full md:w-1/3 hidden md:flex text-text-200 text-2xl md:text-3xl lg:text-5xl  2xl:text-8xl font-semibold transform scale-y-150 origin-bottom">
-            WEB DEVELOPER
-          </p>
-          <div className="w-full md:w-1/3 gap-x-5 flex justify-center md:items-center items-end">
-            <img
+      {/* footer section */}
+      <div className="flex flex-col-reverse gap-y-5 md:flex-row md:items-end  w-full px-7 md:px-0 h-full flex-1">
+        <p className="w-full md:w-1/3 hidden md:flex text-text-200 text-2xl md:text-3xl lg:text-5xl  2xl:text-8xl font-semibold transform scale-y-150 origin-bottom">
+          WEB DEVELOPER
+        </p>
+        <div className="w-full md:w-1/3 gap-x-5 flex justify-center md:items-center items-end">
+          {/* <img
               src="/images/profile.png"
               alt="Profile"
               className="w-30  md:w-80 md:h-100 2xl:w-full 2xl:h-full"
-            />
-            <p className="w-full md:hidden flex text-text-200 text-2xl font-semibold transform scale-y-150 origin-bottom">
-              WEB DEVELOPER
-            </p>
-          </div>
-          <div className="w-full md:w-1/3 flex flex-col items-end md:gap-8">
-            <p className="lg:w-3/4 w-full md:text-end text-lg 2xl:text-2xl text-text-light">
-              I build full-stack web products that are fast, functional, and
-              built to last. Based in Nepal, available globally.
-            </p>
+            /> */}
+          <p className="w-full md:hidden flex text-text-200 text-2xl font-semibold transform scale-y-150 origin-bottom">
+            WEB DEVELOPER
+          </p>
+        </div>
+        <div className="w-full md:w-1/3 flex flex-col items-end md:gap-8">
+          <p className="lg:w-3/4 w-full md:text-end text-lg 2xl:text-2xl text-text-light">
+            Full-stack developer passionate about turning ideas into seamless
+            digital products. Open to remote and international roles.
+          </p>
 
-            <button className=" hover:bg-blue-500 text-primary-foreground shadow-pop group bg-blue-600 cursor-pointer rounded-4xl 2xl:rounded-full text-white text-xl 2xl:text-4xl md:py-5 md:px-7 p-4 2xl:px-20 2xl:py-10 w-fit">
-              <Link
-                to="contact"
-         
-                className="flex items-center"
-              >
-                 CONTACT
-                <ArrowDownRight className="ml-1 h-5 w-5 transition-transform group-hover:rotate-45" />
-              </Link>{" "}
-            </button>
-          </div>
+          <button className=" hover:bg-blue-500 text-primary-foreground shadow-pop group bg-blue-600 cursor-pointer rounded-4xl 2xl:rounded-full text-white text-xl 2xl:text-4xl md:py-5 md:px-7 p-4 2xl:px-20 2xl:py-10 w-fit">
+            <Link to="contact" className="flex items-center">
+              CONTACT
+              <ArrowDownRight className="ml-1 h-5 w-5 transition-transform group-hover:rotate-45" />
+            </Link>{" "}
+          </button>
         </div>
       </div>
     </section>
